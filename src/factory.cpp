e674cbdc1c59c85af92354340d3b85c9d7bddf1b
Roman Kuzmenko <clairbee@guidemaze.com>
2023-03-18 11:47:28 -0700

    refactored the factory interface to enable control of remote behavior for multiple interfaces using the same parameter

--- src/factory.cpp
+++ src/factory.cpp
@@ -16,8 +16,14 @@
 namespace modbus {
 
 std::shared_ptr<Interface> Factory::New(rclcpp::Node *node) {
+  rclcpp::Parameter use_remote;
+  if (!node->has_parameter("use_remote")) {
+    node->declare_parameter("use_remote", true);
+  }
+  node->get_parameter("use_remote", use_remote);
+
   rclcpp::Parameter is_remote;
-  node->declare_parameter("modbus_is_remote", true);
+  node->declare_parameter("modbus_is_remote", use_remote.as_bool());
   node->get_parameter("modbus_is_remote", is_remote);
 
   if (is_remote.as_bool()) {
